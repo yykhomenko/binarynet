2678bfc7c19612344b533629ba2200f9251a2833
yykhomenko <yykhomenko@gmail.com>
2020-12-11 18:01:37 +0200

    added gen

--- main.go
+++ main.go
@@ -7,22 +7,25 @@ func main() {
 	links := genLinks()
 	fmt.Println(nodes)
 
-	nextNodes := genNextNodes(nodes, links)
-	nodes = nextNodes
-
-	fmt.Println(nodes)
+	for i := 0; i < 10; i++ {
+		nextNodes := genNextNodes(nodes, links)
+		nodes = nextNodes
+		fmt.Println(nodes)
+	}
 }
 
 func genNodes() []int {
-	return []int{0, 1, 0, 1}
+	return []int{0, 1, 0, 1, 0, 1}
 }
 
 func genLinks() map[int][]int {
 	return map[int][]int{
 		0: {1, 2},
-		1: {0, 3},
-		2: {0, 1},
-		3: {2, 1},
+		1: {2, 3},
+		2: {1, 1},
+		3: {2, 4},
+		4: {5, 0},
+		5: {1, 3},
 	}
 }
 
@@ -38,11 +41,11 @@ func genNextNodes(nodes []int, links map[int][]int) (out []int) {
 func nextBit(x, y int) int {
 	switch {
 	case x == 0 && y == 0:
-		return 0
+		return 1
 	case x == 0 && y == 1:
 		return 1
 	case x == 1 && y == 0:
-		return 1
+		return 0
 	case x == 1 && y == 1:
 		return 1
 	}
