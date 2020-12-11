5c076938d2851c8c0f4550b65687ce2a5646634a
yykhomenko <yykhomenko@gmail.com>
2020-12-11 17:49:32 +0200

    ref

--- main.go
+++ main.go
@@ -3,30 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	nodes := []bool{false, true}
-	// links := map[byte][]byte{
-	// 	0: {1},
-	// 	1: {0},
-	// }
+	nodes := genNodes()
+	links := genLinks()
 
-	var nextNodes []bool
-	for _, node := range nodes {
-		nextNodes = append(nextNodes, node)
+	var nextNodes []int
+	for i := range nodes {
+		l := links[i]
+		res := nextBit(l[0], l[1])
+		nextNodes = append(nextNodes, res)
 	}
+	nodes = nextNodes
 
 	fmt.Println(nextNodes)
 }
 
-func nextBit(x, y bool) bool {
-	switch {
-	case x == false && y == false:
-		return false
-	case x == false && y == true:
-		return true
-	case x == true && y == false:
-		return true
-	case x == true && y == true:
-		return true
+func genNodes() []int {
+	return []int{0, 1, 0, 1}
+}
+
+func genLinks() map[int][]int {
+	return map[int][]int{
+		0: {1, 2},
+		1: {0, 3},
+		2: {0, 1},
+		3: {2, 1},
 	}
-	return false
+}
+
+func nextBit(x, y int) int {
+	switch {
+	case x == 0 && y == 0:
+		return 0
+	case x == 0 && y == 1:
+		return 1
+	case x == 1 && y == 0:
+		return 1
+	case x == 1 && y == 1:
+		return 1
+	}
+	return 0
 }
